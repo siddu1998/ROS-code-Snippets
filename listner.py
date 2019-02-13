7fbf219a057af7c4258da34f9094002d0634361e
Sai Siddartha Maram <smaram_be16@thapar.edu>
2019-02-13 23:18:26 +0530

    recap

---
+++ listner.py
@@ -0,0 +1,14 @@
+import rospy
+from str_msgs import String
+
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + " I heard this :" + data.data)
+
+def listener():
+    rospy.init_node('listener',anonymous=True)
+    rospy.Subscriber('chatter',String,callback)
+    rospy.spin()
+
+if __name__=='__main__':
+    listener()
